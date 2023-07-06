4421c06e24d1e7f73f31257f60917e3d3bfa7811
Kishore <Kishore@LAPTOP-P7130GQF>
2023-07-06 16:32:30 +0530

    This commit is consist of the reporting statement in TC_01_Test

--- demowebshop/src/test/java/login/TC_01_Test.java
+++ demowebshop/src/test/java/login/TC_01_Test.java
@@ -1,5 +1,6 @@
 package login;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import genericutility.BaseClass;
@@ -8,6 +9,7 @@ public class TC_01_Test extends BaseClass{
 	@Test
 public void tc() {
 	login.getLoginLink().click();
+	Reporter.log("for demo");
 	login.getEmailTF().sendKeys("mamatha.kr@testyantra.com");
 	login.getPasswordTF().sendKeys("123456789");
 	login.getLoginButton().click();
